c90b3e405348f021f2038c30c0e497d98967269e
Emma Vedel <emmavedelpedersen@gmail.com>
2026-01-15 22:46:12 +0100

    re-add default to card size

--- functions/displaycard.lua
+++ functions/displaycard.lua
@@ -1,15 +1,20 @@
 PokeDisplayCard = Card:extend()
 
 function PokeDisplayCard:init(args, x, y, w, h)
+  w = w or args.w or G.CARD_W
+  h = h or args.h or G.CARD_H
+
   local properties = {
     atlas = args.atlas,
     pos = args.pos,
     soul_pos = args.soul_pos,
   }
 
-  local fake_center = setmetatable({}, {__index = function(_table, key)
-    return properties[key] or G.P_CENTERS.j_joker[key]
-  end})
+  local fake_center = setmetatable({}, {
+    __index = function(_table, key)
+      return properties[key] or G.P_CENTERS.j_joker[key]
+    end
+  })
 
   Card.init(self, x, y, w, h, nil, fake_center)
 end
