e70393bbbe3f0de4ba2ac72c785c3aa483ca6a32
Emma Vedel <emmavedelpedersen@gmail.com>
2026-01-15 23:30:51 +0100

    re-add controller highlight support, some things never change

--- functions/displaycard.lua
+++ functions/displaycard.lua
@@ -22,3 +22,24 @@ function PokeDisplayCard:init(args, x, y, w, h)
 
   self.sticker_run = 'NONE'
 end
+
+-- Controller support
+local is_node_focusable_ref = G.CONTROLLER.is_node_focusable
+function G.CONTROLLER:is_node_focusable(node)
+  if not self.screen_keyboard and node:is(PokeDisplayCard) then
+    return true
+  end
+  return is_node_focusable_ref(self, node)
+end
+
+local game_draw_ref = Game.draw
+function Game:draw()
+  game_draw_ref(self)
+  local target = self.CONTROLLER.focused.target
+  if target and target:is(PokeDisplayCard) then
+    love.graphics.push()
+    target:translate_container()
+    target:draw()
+    love.graphics.pop()
+  end
+end
